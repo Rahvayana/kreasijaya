349758f94756b764210890253f79352da30ceff7
Rahvayana <fahrursanjaya@gmail.com>
2021-11-17 19:44:07 +0700

    indexing google

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -32,6 +32,7 @@
   * Author: BootstrapMade.com
   * License: https://bootstrapmade.com/license/
   ======================================================== -->
+  <meta name="google-site-verification" content="W2w3M_RLpUgpjKtgSLaCD3bZlYoKkGfGDDgm5MiOkmc" />
 </head>
 
 <body>
@@ -166,6 +167,17 @@
 
   <!-- Template Main JS File -->
   <script src="/assets/js/main.js"></script>
+  <!-- Google Analytics -->
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+    ga('create', 'UA-XXXXX-Y', 'auto');
+    ga('send', 'pageview');
+  </script>
+  <!-- End Google Analytics -->
 
 </body>
 
